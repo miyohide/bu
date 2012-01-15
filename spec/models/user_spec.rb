5db22ff8d9df65df558a77f7f878afabb544b776
miyohide <miyohide@gmail.com>
2012-01-15 15:30:04 +0900

    userモデルのテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,49 @@
+# coding: UTF-8
+
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+   describe "を登録するとき" do
+      it "空欄のためにバリデーションに失敗すること" do
+         @user = User.new(:name => "")
+         @user.should_not be_valid
+      end
+
+      it "長さが長すぎるためにバリデーションに失敗すること" do
+         @user = User.new(:name => "12345678901234567")
+         @user.should_not be_valid
+      end
+
+      it "名前が入力されてあって、長さが適切であれば登録できること" do
+         @user = User.new(:name => "1234567890123456")
+         @user.should be_valid
+      end
+
+   end
+
+   describe "権限をチェックするときに" do
+      fixtures :users
+      it "adminはtrueが返ること" do
+         @user = users(:admin)
+         @user.administrator?.should be_true
+      end
+
+      it "adminでなければfalseが返ること" do
+         @user = users(:user1)
+         @user.administrator?.should be_false
+      end
+
+   end
+
+   describe "statusの確認をするとき" do
+      it "常にnewが返ること" do
+         @user = User.new
+         @user.status.should == "new"
+      end
+
+
+   end
+
+
+
 end
